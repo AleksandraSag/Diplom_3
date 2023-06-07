9234272d1c6ee17e657e12268f3014c89df718e4
Alexandra <aleksandra.sagaidak@yandex.ru>
2023-06-07 04:14:06 +0300

    commit_Diplom_3

---
+++ src/test/java/SingOutTest.java
@@ -0,0 +1,24 @@
+import account.ProfilePage;
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import login.LoginPage;
+import model.MainPage;
+import org.junit.Test;
+
+public class SingOutTest extends TestBase {
+
+    @Test
+    @DisplayName("Выход из аккаунта авторизованного пользователя")
+    @Description("Проверка, что можно выйти из аккаунта авторизованного пользователя")
+    public void canSingOutFromProfile() {
+        MainPage mainPage = new MainPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        ProfilePage profilePage = new ProfilePage(driver);
+
+        userApi.userReg(user);
+        loginPage.setSingInUserAccount(user.getEmail(), user.getPassword());
+        mainPage.clickPersonalAccountBtn();
+        profilePage.clickSingOutBtn();
+        profilePage.checkAccountTextIsDisplayed();
+    }
+}
